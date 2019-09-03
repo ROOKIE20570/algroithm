8a37790f09d17b0f56071a185ab6da9d71e0e70e
rookie20570 <lxiaoyi@rolaner.com>
2019-09-03 11:46:11 +0800

    list

--- list/singleList.go
+++ list/singleList.go
@@ -14,14 +14,20 @@ type linkedList struct {
 
 func main() {
 	list := initList()
+
 	list.insertAfter(list.head, 5)
 	list.insertAfter(list.head, 6)
 	list.insertAfter(list.head, 7)
 	list.insertAfter(list.head, 8)
 
-	list.printList()
-	list.reserver()
-	list.printList()
+	list1 := initList()
+	list1.insertAfter(list1.head, 3)
+	list1.insertAfter(list1.head, 6)
+	list1.insertAfter(list1.head, 11)
+	list1.insertAfter(list1.head, 12)
+
+	MergeSortedList(list1,list).printList()
+
 
 }
 
@@ -31,6 +37,10 @@ func initList() *linkedList {
 
 func newListNode(val int) *listNode {
 	return &listNode{nil, val}
+}
+
+func (link *linkedList)newEndNode(val int) *listNode {
+	return &listNode{link.head, val}
 }
 
 func (list *linkedList) insertAfter(node *listNode, val int) bool {
@@ -67,7 +77,7 @@ func (list *linkedList) reserver() {
 	cur := list.head.next //暂存当前遍历到的结点
 
 	for cur != nil {
-		//从后向前摆放
+		//首先把下一个要操作的结点记下来
 		tmp := cur.next
 		cur.next = pre
 		pre = cur
@@ -77,4 +87,78 @@ func (list *linkedList) reserver() {
 	list.head.next = pre
 }
 
+func (list *linkedList) HasCycle() bool {
+	if nil != list.head {
+		fast, slow := list.head, list.head
+
+		for fast != nil && fast.next != nil {
+			fast = fast.next.next
+			slow = slow.next
+
+			if slow == fast {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func (this *linkedList) deleteBottomN(n int) {
+	if n <= 0 || nil == this.head || nil == this.head.next {
+		return
+	}
+
+	fast := this.head
+	for i := 0; fast != nil && i < n; i++ {
+		fast = fast.next
+	}
+
+	if nil == fast{
+		return
+	}
+
+	slow := this.head
+	for fast.next != nil  {
+		fast = fast.next
+		slow = slow.next
+	}
+
+	slow.next = slow.next.next	
+
+}
+
+func MergeSortedList(l1, l2 *linkedList) *linkedList {
+	if nil == l1 || nil == l1.head || nil == l1.head.next {
+		return l2
+	}
+	if nil == l2 || nil == l2.head || nil == l2.head.next {
+		return l1
+	}
+
+	l := &linkedList{head: &listNode{}}
+	cur := l.head
+	curl1 := l1.head.next
+	curl2 := l2.head.next
+	for nil != curl1 && nil != curl2 {
+		if curl1.val < curl2.val {
+			cur.next = curl2
+			curl2 = curl2.next
+		} else {
+			cur.next = curl1
+			curl1 = curl1.next
+		}
+		cur = cur.next
+	}
+
+	if nil != curl1 {
+		cur.next = curl1
+	} else if nil != curl2 {
+		cur.next = curl2
+	}
+
+
+
+	return l
+}
 
