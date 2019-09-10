920f785e043c1e50b9daf3f680ed25c724bc98a4
rookie20570 <lxiaoyi@rolaner.com>
2019-09-10 11:59:25 +0800

    heap

--- heap/heap.go
+++ heap/heap.go
@@ -34,9 +34,36 @@ func (h *heap) insert(data int) {
 		h.content[i], h.content[i/2] = h.content[i/2], h.content[i]
 		i = i / 2
 	}
-	
+
 }
 
-func remove()  {
-	
+func (h *heap) remove() {
+	if (h.n == 0) {
+		return
+	}
+
+	h.content[1] = h.content[h.n]
+	h.n--
+	heapify(h.content,h.n,1)
+}
+
+func heapify(arr []int, n, i int) {
+	for {
+		maxPos := i
+		if i*2 <= n && arr[maxPos] < arr[i*2] {
+			maxPos = i * 2
+		}
+
+		if i*2+1 <= n && arr[maxPos] < arr[i*2+1] {
+			maxPos = i*2 + 1
+		}
+
+		if maxPos == i {
+			break
+		}
+
+		arr[i],arr[maxPos] = arr[maxPos],arr[i]
+
+		i = maxPos
+	}
 }
