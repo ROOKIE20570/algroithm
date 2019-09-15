249df0752821ce4fa846aa7fb5261cdba2f3f707
lixiaoyi <422567988@qq.com>
2019-09-15 15:53:23 +0800

    fix

--- stack/stackArr.go
+++ stack/stackArr.go
@@ -1,9 +1,44 @@
 package main
 
-import "fmt"
+type stack struct {
+	len  int
+	top  int
+	data []int
+}
 
 func main()  {
-	c := make([]int,0,32)
-	fmt.Println(c)
+
+}
+func initStack(cap int) *stack {
+	s := new(stack)
+	s.len = cap
+	s.top = -1
+
+	return s
+
+}
+
+func (s *stack) isFull() {
+
+}
+func (s *stack) push(item int) bool {
+	if s.top+1 == s.len {
+		return false
+	}
+
+	s.top++
+	s.data[s.top] = item
+
+	return true
+}
+
+func (s *stack) pop() int {
+	if s.top == -1 {
+		return 0
+	}
+	res := s.data[s.top]
+	s.top--
+
+	return res
 }
 
