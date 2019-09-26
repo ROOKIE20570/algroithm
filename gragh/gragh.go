d8eb0abddd201fe60ecf28b52f4c7049df495882
rookie20570 <rookie20570@gmail.com>
2019-09-26 18:24:55 +0800

    new gragh

--- gragh/gragh.go
+++ gragh/gragh.go
@@ -1,16 +1,39 @@
 package main
 
-import "container/list"
+import (
+	"container/list"
+	"fmt"
+)
 
 type gragh struct {
 	vertex int
-	data []map[int]int
-
-}
-func main()  {
-	list.New()
+	data   []*list.List
 }
 
-func newGragh()  {
+func main() {
+	myGragh := newGragh(5)
+	fmt.Println(myGragh)
+}
+
+func newGragh(v int) *gragh {
+	myGragh := new(gragh)
+	myGragh.vertex = v
+	myGragh.data = make([]*list.List, v)
+	for i := 0; i < v; i++ {
+		myGragh.data[i] = new(list.List)
+	}
+	return myGragh
+}
+
+func (myGragh *gragh)addEdge(s, t int)  {
+	myGragh.data[s].PushBack(t)
+	myGragh.data[t].PushBack(s)
+}
+
+func bfs(s, t int)  {
 
 }
+
+
+
+
