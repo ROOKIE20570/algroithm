b67f6f5562fd883a1ecf0477b6d9b799372acdc9
rookie20570 <rookie20570@gmail.com>
2019-09-26 20:15:36 +0800

    bfs

--- gragh/gragh.go
+++ gragh/gragh.go
@@ -25,15 +25,67 @@ func newGragh(v int) *gragh {
 	return myGragh
 }
 
-func (myGragh *gragh)addEdge(s, t int)  {
+func (myGragh *gragh) addEdge(s, t int) {
 	myGragh.data[s].PushBack(t)
 	myGragh.data[t].PushBack(s)
 }
 
-func bfs(s, t int)  {
+//思路 visted 已经遍历过的顶点 prev[] 记录路径   queue 存储前一层的结点
+func (myGragh *gragh) bfs(s, t int) {
+	//一千个伤心的理由
+	if s == t {
+		return
+	}
+
+	visted := make([]bool, myGragh.vertex)
+	//队列  记录的
+	stash := make([]int, myGragh.vertex)
+	prev := make([]int, myGragh.vertex)
+	//
+	for index := range prev {
+		prev[index] = -1
+	}
+
+	stash = append(stash, s)
+
+	visted[s] = true
+	isFound := false
+	for len(stash) > 0 {
+		t := stash[0]
+		stash = stash[1:]
+		cur := myGragh.data[t]
+		for e := cur.Front(); e != nil; e = e.Next() {
+			k := e.Value.(int)
+			if !visted[k] {
+				prev[k] = t
+				if k == t {
+					isFound = true
+					break
+				}
+
+				stash = append(stash,k)
+				visted[k] = true
+			}
+		}
+
+	}
+
+	if isFound {
+		printPrev(prev,s,t)
+	}else {
+		fmt.Println("error")
+	}
+	//visted[s] = true
 
 }
 
+func printPrev(prev []int, s int, t int) {
 
+	if t == s || prev[t] == -1 {
+		fmt.Printf("%d ", t)
+	} else {
+		printPrev(prev, s, prev[t])
+		fmt.Printf("%d ", t)
+	}
 
-
+}
